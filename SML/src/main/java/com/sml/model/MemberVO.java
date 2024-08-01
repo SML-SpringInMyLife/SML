66bcad6a7d022d3d93afb90b7b4acd81356dd71f
DESKTOP-E3PHQHP\JIN <jin22636@gmail.com>
2024-08-01 20:40:16 +0900

    member 업데이트

--- SML/src/main/java/com/sml/model/MemberVO.java
+++ SML/src/main/java/com/sml/model/MemberVO.java
@@ -1,93 +1,43 @@
 package com.sml.model;
 
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
 public class MemberVO {
-	
-	private String memberId;
-	private String memberPw;
-	private String memberName;
-	private String memberMail;
-	private String memberAddr1;
-	private String memberAddr2;
-	private String memberAddr3;
-	private int adminCk;
-	private int regDate;
-	private int money;
-	private int point;
+	//회원가입 입력사항 : 아이디,비밀번호,이름,메일,연락처,비상연락처,주소,생년월일
+	private Long memCode;
+	private String memId;
+	private String memPw;
+    private String memName;
+    private String memMail;
+    private String memPhone;
+    private String memEmerPhone;
+    
+    //회원 주소
+    private String memAddr1;
+    private String memAddr2;
+    private String memAddr3;
+    
+    //회원 생년월일
+    private Date memBirth;
+    
+    //회원 가입일
+    private Date memJoinDate;
+    
+    //회원 탈퇴일
+    private Date memQuitDate;
+    
+    private Integer memStatus;
+    
+    //회원 포인트
+    private Integer memTotalPoint;
+    
+    //관리자 구분(0:일반사용자, 1:관리자)
+    private Integer memAdminCheck;
+    
+    private Integer pointCode;
 
-	test
 	
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-	public String getMemberPw() {
-		return memberPw;
-	}
-	public void setMemberPw(String memberPw) {
-		this.memberPw = memberPw;
-	}
-	public String getMemberName() {
-		return memberName;
-	}
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
-	}
-	public String getMemberMail() {
-		return memberMail;
-	}
-	public void setMemberMail(String memberMail) {
-		this.memberMail = memberMail;
-	}
-	public String getMemberAddr1() {
-		return memberAddr1;
-	}
-	public void setMemberAddr1(String memberAddr1) {
-		this.memberAddr1 = memberAddr1;
-	}
-	public String getMemberAddr2() {
-		return memberAddr2;
-	}
-	public void setMemberAddr2(String memberAddr2) {
-		this.memberAddr2 = memberAddr2;
-	}
-	public String getMemberAddr3() {
-		return memberAddr3;
-	}
-	public void setMemberAddr3(String memberAddr3) {
-		this.memberAddr3 = memberAddr3;
-	}
-	public int getAdminCk() {
-		return adminCk;
-	}
-	public void setAdminCk(int adminCk) {
-		this.adminCk = adminCk;
-	}
-	public int getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(int regDate) {
-		this.regDate = regDate;
-	}
-	public int getMoney() {
-		return money;
-	}
-	public void setMoney(int money) {
-		this.money = money;
-	}
-	public int getPoint() {
-		return point;
-	}
-	public void setPoint(int point) {
-		this.point = point;
-	}
-	@Override
-	public String toString() {
-		return "MemberVO [memberId=" + memberId + ", memberPw=" + memberPw + ", memberName=" + memberName
-				+ ", memberMail=" + memberMail + ", memberAddr1=" + memberAddr1 + ", memberAddr2=" + memberAddr2
-				+ ", memberAddr3=" + memberAddr3 + ", adminCk=" + adminCk + ", regDate=" + regDate + ", money=" + money
-				+ ", point=" + point + "]";
-	}
-
 }
