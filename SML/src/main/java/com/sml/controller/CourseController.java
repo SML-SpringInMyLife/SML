724e90a46d34805167d2a0bdd7c9ae87404b6ee9
kosm0ssy <xzerokim@gmail.com>
2024-08-01 17:29:57 +0900

    #13 fix 게시판 mapper test 오류

--- SML/src/main/java/com/sml/controller/CourseController.java
+++ SML/src/main/java/com/sml/controller/CourseController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sml.service.CourseService;
@@ -13,7 +14,7 @@ import com.sml.service.CourseService;
 @RequestMapping("/course")
 public class CourseController {
 	
-private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
 	
 	@Autowired
 	private CourseService service;
@@ -27,5 +28,24 @@ private static final Logger logger = LoggerFactory.getLogger(AdminController.cla
 	public void courseEnrollGET() throws Exception{
 		logger.info("수업 등록 페이지 이동");
 	}
+	@PostMapping("/enroll")
+	public String enrollPOST() throws Exception{
+		return "redirect:/course/boardList";
+	}
+	
+	@GetMapping("/modify")
+	   public void modifyGET() throws Exception{
+		   logger.info("수업 수정");
+	   }
+	   @PostMapping("/modify")
+	   public String modifyPOST() throws Exception{
+		   //해당 페이지로 다시 return - 바꾸기 
+		   return "redirect:/course/boardList";
+	   }
+	   
+	   @PostMapping("/delete")
+	   public String deletePOST() throws Exception{
+		   return "redirect:/course/boardList";
+	   }
 
 }
