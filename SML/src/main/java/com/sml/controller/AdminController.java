1283e0b222b5efe9fc7700056e35155298b61e27
EunahPae <eunahp86@gmail.com>
2024-08-21 20:30:30 +0900

    #46 3일연속 미출석 자동 문자 발송 기능 구현

--- SML/src/main/java/com/sml/controller/AdminController.java
+++ SML/src/main/java/com/sml/controller/AdminController.java
@@ -167,8 +167,8 @@ public class AdminController {
 		return "redirect:/admin/sms";
 	}
 
-	// 3일 연속 미출석 회원에게 안부 문자 발송 (매일 7:27 PM)
-	@Scheduled(cron = "0 13 20 * * ?")
+	// 3일 연속 미출석 회원에게 안부 문자 발송 (매일 10:00 AM)
+	@Scheduled(cron = "0 0 10 * * ?")
 	public void sendReminderSmsToAbsentMembers() {
 		logger.info("3일 연속 미출석 안부문자 발송일시 : {}", new Date());
 		try {
